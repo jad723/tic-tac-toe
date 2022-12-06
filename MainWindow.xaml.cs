01b0cf3441021dd0783416f3b89c6fa5f9bb25e6
jad723 <jadmaalouf2003.jm15@gmail.com>
2022-12-06 11:47:48 +0200

    Initial commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
@@ -20,9 +11,63 @@ namespace Tic_Tac_Toe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Private members
+
+        /// <summary>
+        /// Holds the current cell results of cells in the active game
+        /// </summary>
+        private MarkType[] mResults;
+
+        /// <summary>
+        /// True if it is player 1's turn (X) or player 2's turn (O)
+        /// </summary>
+        private bool mPlayer1Turn;
+
+        /// <summary>
+        /// True if the game has ended
+        /// </summary>
+        private bool mGameEnded;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+
         public MainWindow()
         {
             InitializeComponent();
+
+            NewGame();
+        }
+
+        #endregion
+
+        private void NewGame()
+        {
+            // Create new blank array of free cells
+            mResults = new MarkType[9];
+
+            for(var i=0; i < mResults.Length; i++) mResults[i] = MarkType.Free;
+
+            //Make sure Player 1 starts the game
+            mPlayer1Turn = true;
+
+            // Iterate every button on the grid...
+            Container.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                // Change background, foreground and content to default values
+                button.Content = string.Empty;
+                button.Background = Brushes.White;
+                button.Foreground = Brushes.Blue;
+            });
+
+            // Make sure game hasn't finished
+            mGameEnded = false;
+
+            
         }
     }
 }
